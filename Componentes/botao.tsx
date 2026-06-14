809e926c2a5af84db893eedd523314699796d530
origamid55 <reserva7408@gmail.com>
2026-06-14 00:22:12 -0300

    commit

--- Componentes/botao.tsx
+++ Componentes/botao.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import { ReactNode, MouseEvent } from 'react';
 
-const botao = ({children}, {onClick}) => {
+// Definimos os tipos exatos para o children e para o onClick
+interface BotaoProps {
+  children: ReactNode;
+  onClick: (event: MouseEvent<HTMLButtonElement>) => void; 
+}
+
+const Botao = ({ children, onClick }: BotaoProps) => {
   return (
      <button onClick={onClick}> {children} </button>
   )
 }
 
-export default botao;
+export default Botao;
